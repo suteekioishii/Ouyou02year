2cb35f4090e445968795229dd381e750b78e5878
Naoto Tokunaga <ne221156@senshu-u.jp>
2024-01-06 00:18:13 +0900

    cssちょっと加えた

--- db/seeds/development/shifts.rb
+++ db/seeds/development/shifts.rb
@@ -1,12 +1,14 @@
-start = DateTime.new(2024,1,7,0,0,0)
-
 0.upto(9) do |idx|
-    0.upto(14*48) do |time|
-        Shift.create!(
-            stylist_id: idx + 1,
-            reservation_id: 1,
-            date_time: start.since(30.minutes*time)
-        )
+    0.upto(13) do |days|
+        #初期データは9時開業の12時間営業
+        start = DateTime.new(2024,1,7+days,9,0,0)
+        0.upto(23) do|minute|
+            Shift.create!(
+                stylist_id: idx + 1,
+                reservation_id: 1,
+                date_time: start.since(30.minutes*minute)
+            )
+        end
     end
 end
 
